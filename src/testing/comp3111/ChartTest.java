a076f9c5f1ff1aac19fbe03dc44f41ff5c3927f5
Bill Chan <billpwchan@hotmail.com>
2018-04-30 20:05:57 +0800

    Complete 100% coverage in core folder.

--- src/testing/comp3111/ChartTest.java
+++ src/testing/comp3111/ChartTest.java
@@ -6,14 +6,16 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import core.comp3111.Chart;
+
 import core.comp3111.AnimatedLineChart;
 import core.comp3111.BarChartP;
-import core.comp3111.LineChartP;
+import core.comp3111.Chart;
+import core.comp3111.DataColumn;
 import core.comp3111.DataTable;
 import core.comp3111.DataTableException;
-import core.comp3111.DataColumn;
 import core.comp3111.DataType;
+import core.comp3111.LineChartP;
+
 /**
  * 
  * @author wonyoung1026
@@ -21,143 +23,141 @@ import core.comp3111.DataType;
  */
 public class ChartTest {
 	DataTable dt, X, Y;
-	String title; 
+	String title;
 	Chart lineChartSimple, animatedChartSimple, barChartSimple, lineChart, animatedChart, barChart;
+
 	@BeforeEach
 	void init() {
 		dt = new DataTable();
 		X = new DataTable();
 		Y = new DataTable();
 		title = new String();
-		
+
 		lineChartSimple = new LineChartP(dt);
 		animatedChartSimple = new AnimatedLineChart(dt);
 		barChartSimple = new BarChartP(dt);
-		
-		lineChart = new LineChartP(dt, X,Y,title);
-		animatedChart = new AnimatedLineChart(dt, X,Y,title);
-		barChart = new BarChartP(dt, X,Y,title);
+
+		lineChart = new LineChartP(dt, X, Y, title);
+		animatedChart = new AnimatedLineChart(dt, X, Y, title);
+		barChart = new BarChartP(dt, X, Y, title);
 	}
+
 	@Test
 	void testChartConstructor() {
 	}
-	
+
 	@Test
 	void testGetType() {
-		assert(lineChart.getTypeID() == 0);
-		assert(barChart.getTypeID() == 1);
-		assert(animatedChart.getTypeID() == 2);
-		
+		assert (lineChart.getTypeID() == 0);
+		assert (barChart.getTypeID() == 1);
+		assert (animatedChart.getTypeID() == 2);
+
 	}
 
-	
 	@Test
 	void testLineChartDataRequirementValidation() throws DataTableException {
 		Number[] arrNum = new Number[] { 1, 4, 2, 3, 5, 0, 0 };
 		DataColumn dcNum = new DataColumn(DataType.TYPE_NUMBER, arrNum);
-		
+
 		Number[] arrNum2 = new Number[] { 1, 4, 2, 3, 5, 7, 8 };
 		DataColumn dcNum2 = new DataColumn(DataType.TYPE_NUMBER, arrNum2);
-		
+
 		dt.addCol("dc1", dcNum);
 		lineChart = new LineChartP(dt, X, Y, title);
-		assert(lineChart.dataRequirementValidation() == false);
-		
+		assert (lineChart.dataRequirementValidation() == false);
+
 		dt.addCol("dc2", dcNum2);
 		lineChart = new LineChartP(dt, X, Y, title);
-		assert(lineChart.dataRequirementValidation() == true);		
+		assert (lineChart.dataRequirementValidation() == true);
 	}
-	
+
 	@Test
-	void testBarChartDataRequirementValidation() throws DataTableException{
-		Number[] arrNum = new Number[] { 1, 4, 2};
+	void testBarChartDataRequirementValidation() throws DataTableException {
+		Number[] arrNum = new Number[] { 1, 4, 2 };
 		DataColumn dcNum = new DataColumn(DataType.TYPE_NUMBER, arrNum);
-		
-		String[] arrStr = new String[] { "ab", "cd", "ef"};
+
+		String[] arrStr = new String[] { "ab", "cd", "ef" };
 		DataColumn dcStr = new DataColumn(DataType.TYPE_STRING, arrStr);
-		
-		assert(barChart.dataRequirementValidation() == false);
-		
+
+		assert (barChart.dataRequirementValidation() == false);
+
 		dt.addCol("str", dcStr);
-		assert(barChart.dataRequirementValidation() == false);
-		
+		assert (barChart.dataRequirementValidation() == false);
+
 		dt.addCol("num", dcNum);
-		assert(barChart.dataRequirementValidation() == true);
+		assert (barChart.dataRequirementValidation() == true);
 	}
-	
+
 	@Test
-	void testAnimatedLineChartDataRequirementValidation() throws DataTableException{
+	void testAnimatedLineChartDataRequirementValidation() throws DataTableException {
 		Number[] arrNum = new Number[] { 1, 4, 2, 3, 5, 0, 0 };
 		DataColumn dcNum = new DataColumn(DataType.TYPE_NUMBER, arrNum);
-		
+
 		Number[] arrNum2 = new Number[] { 1, 4, 2, 3, 5, 7, 8 };
 		DataColumn dcNum2 = new DataColumn(DataType.TYPE_NUMBER, arrNum2);
-		
+
 		dt.addCol("dc1", dcNum);
 		animatedChart = new AnimatedLineChart(dt, X, Y, title);
-		assert(animatedChart.dataRequirementValidation() == false);
-		
+		assert (animatedChart.dataRequirementValidation() == false);
+
 		dt.addCol("dc2", dcNum2);
 		animatedChart = new AnimatedLineChart(dt, X, Y, title);
-		assert(animatedChart.dataRequirementValidation() == true);	
-		
+		assert (animatedChart.dataRequirementValidation() == true);
+
 	}
-	
+
 	@Test
 	void testGetDataTable() {
-		assert(lineChart.getDataTable(0) == dt);
-		assert(lineChart.getDataTable(1) == X);
-		assert(lineChart.getDataTable(2) == Y);
-		assert(lineChart.getDataTable(3) == null);
+		assert (lineChart.getDataTable(0) == dt);
+		assert (lineChart.getDataTable(1) == X);
+		assert (lineChart.getDataTable(2) == Y);
+		assert (lineChart.getDataTable(3) == null);
 	}
-	
+
 	@Test
 	void testGetTitle() {
 		assertEquals(lineChart.getTitle(), title + " line chart");
 	}
-	
+
 	@Test
 	void testColXSize() {
-		assert(lineChart.colXSize() == 0);
+		assert (lineChart.colXSize() == 0);
 	}
 
 	@Test
 	void testColYSize() {
-		assert(lineChart.colYSize() == 0);
+		assert (lineChart.colYSize() == 0);
 	}
-	
+
 	@Test
 	void testChartGetColX() throws DataTableException {
 		Number[] arrNum = new Number[] { 1, 4, 2, 3, 5, 0, 0 };
 		DataColumn dcNum = new DataColumn(DataType.TYPE_NUMBER, arrNum);
 		X.addCol("dc1", dcNum);
-		
-		assert(lineChart.chartGetColX(X.getNumCol()+1)==null);
-		assert(lineChart.chartGetColX(X.getNumCol())==null);
-		assert(lineChart.chartGetColX(-1)==null);
-		
-		
+
+		assert (lineChart.chartGetColX(X.getNumCol() + 1) == null);
+		assert (lineChart.chartGetColX(X.getNumCol()) == null);
+		assert (lineChart.chartGetColX(-1) == null);
+
 		List<DataColumn> tempDC = X.getAllColValue();
-		assert(lineChart.chartGetColX(0) == dcNum);
-		assert(tempDC.get(0) == dcNum);
+		assert (lineChart.chartGetColX(0) == dcNum);
+		assert (tempDC.get(0) == dcNum);
 	}
-	
+
 	@Test
-	void testChartGetColY() throws DataTableException{
+	void testChartGetColY() throws DataTableException {
 		Number[] arrNum = new Number[] { 1, 4, 2, 3, 5, 0, 0 };
 		DataColumn dcNum = new DataColumn(DataType.TYPE_NUMBER, arrNum);
 		Y.addCol("dc1", dcNum);
-		
-		assert(lineChart.chartGetColY(Y.getNumCol()+1)==null);
-		assert(lineChart.chartGetColY(Y.getNumCol())==null);
-		assert(lineChart.chartGetColY(-1)==null);
-		
-		
+
+		assert (lineChart.chartGetColY(Y.getNumCol() + 1) == null);
+		assert (lineChart.chartGetColY(Y.getNumCol()) == null);
+		assert (lineChart.chartGetColY(-1) == null);
+
 		List<DataColumn> tempDC = Y.getAllColValue();
-		assert(lineChart.chartGetColY(0) == dcNum);
-		assert(tempDC.get(0) == dcNum);
-		
+		assert (lineChart.chartGetColY(0) == dcNum);
+		assert (tempDC.get(0) == dcNum);
+
 	}
-	
-	
+
 }
--- src/ui/comp3111/CustomFileChooser.java
+++ src/ui/comp3111/CustomFileChooser.java
@@ -11,53 +11,75 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class CustomFileChooser {
-	//Attributes
+	// Attributes
 	private File file;
 	private FileChooser fc;
-	
-	
+
 	/**
-	 * @param Stage object
-	 * @param Set title of the FileChooser
-	 * @param Set ExtensionFilterTitle
-	 * @param Set ExtensionFilterAbrv.
-	 * @param Set Initial File Path
+	 * @param Stage
+	 *            object
+	 * @param Set
+	 *            title of the FileChooser
+	 * @param Set
+	 *            ExtensionFilterTitle
+	 * @param Set
+	 *            ExtensionFilterAbrv.
+	 * @param Set
+	 *            Initial File Path
 	 */
-	public void LoadFileChooser(Stage stage, String title, String ExtensionFilterTitle, String ExtensionFilterFormat, String InitDir) {
+	public void LoadFileChooser(Stage stage, String title, String ExtensionFilterTitle, String ExtensionFilterFormat,
+			String InitDir) {
 		this.fc = new FileChooser();
 		this.generalFileChooser(stage, title, ExtensionFilterTitle, ExtensionFilterFormat, InitDir, 1);
 	}
-	
+
 	/**
-	 * @param Stage object
-	 * @param Set title of the FileChooser
-	 * @param Set ExtensionFilterTitle
-	 * @param Set ExtensionFilterAbrv.
-	 * @param Set Initial File Path
-	 * @param Set Initial File Name
+	 * @param Stage
+	 *            object
+	 * @param Set
+	 *            title of the FileChooser
+	 * @param Set
+	 *            ExtensionFilterTitle
+	 * @param Set
+	 *            ExtensionFilterAbrv.
+	 * @param Set
+	 *            Initial File Path
+	 * @param Set
+	 *            Initial File Name
 	 */
-	public void saveFileChooser(Stage stage, String title, String ExtensionFilterTitle, String ExtensionFilterFormat, String InitDir, String InitFileName) {
+	public void saveFileChooser(Stage stage, String title, String ExtensionFilterTitle, String ExtensionFilterFormat,
+			String InitDir, String InitFileName) {
 		this.fc = new FileChooser();
 		fc.setInitialFileName(InitFileName);
 		this.generalFileChooser(stage, title, ExtensionFilterTitle, ExtensionFilterFormat, InitDir, 2);
 	}
-	
+
 	/**
-	 * @param Stage object
-	 * @param Set title of the FileChooser
-	 * @param Set ExtensionFilterTitle
-	 * @param Set ExtensionFilterAbrv.
-	 * @param Set Initial File Path
+	 * @param Stage
+	 *            object
+	 * @param Set
+	 *            title of the FileChooser
+	 * @param Set
+	 *            ExtensionFilterTitle
+	 * @param Set
+	 *            ExtensionFilterAbrv.
+	 * @param Set
+	 *            Initial File Path
 	 */
-	private void generalFileChooser(Stage stage, String title, String ExtensionFilterTitle, String ExtensionFilterFormat, String InitDir, int Option) {
+	private void generalFileChooser(Stage stage, String title, String ExtensionFilterTitle,
+			String ExtensionFilterFormat, String InitDir, int Option) {
 		fc.setTitle(title);
 		fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter(ExtensionFilterTitle, ExtensionFilterFormat));
 		fc.setInitialDirectory(new File(System.getProperty(InitDir)));
-		
-		if (Option == 1) {this.file = fc.showOpenDialog(stage);}
-		if (Option == 2) {this.file = fc.showSaveDialog(stage);}
+
+		if (Option == 1) {
+			this.file = fc.showOpenDialog(stage);
+		}
+		if (Option == 2) {
+			this.file = fc.showSaveDialog(stage);
+		}
 	}
-	
+
 	/**
 	 * @return
 	 */
@@ -80,11 +102,9 @@ public class CustomFileChooser {
 
 		Object[] possibilities = { "Replace with Mean", "Replace with Median", "Replace with Zero" };
 
-		return JOptionPane.showOptionDialog(frame,
-				"Please preferred way for replacing missing numerical values", "Please select...",
-				JOptionPane.INFORMATION_MESSAGE, 1, icon, possibilities, 0);
+		return JOptionPane.showOptionDialog(frame, "Please preferred way for replacing missing numerical values",
+				"Please select...", JOptionPane.INFORMATION_MESSAGE, 1, icon, possibilities, 0);
 	}
-
 
 	/**
 	 * @return the file
@@ -93,9 +113,9 @@ public class CustomFileChooser {
 		return file;
 	}
 
-
 	/**
-	 * @param file the file to set
+	 * @param file
+	 *            the file to set
 	 */
 	public void setFile(File file) {
 		this.file = file;
